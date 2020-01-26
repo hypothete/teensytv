68969386f9687ec08211f81f14ef0535d55bd1d9
Duncan Alexander <hypo@hypothete.com>
2020-01-26 13:16:43 -0800

    figured out how to get 1ms delays working

--- tv-test.ino
+++ tv-test.ino
@@ -22,7 +22,7 @@ void setupBuffer() {
     int dx = W2 - x;
     int dy = H2 - y;
     int hypo = sqrt(dx*dx + dy*dy);
-    dst_buf[i] = hypo < 16 * (sin((float)(frameCount / 32.0)) + 1);
+    dst_buf[i] = hypo < 12; //16 * (sin((float)(frameCount / 32.0)) + 1);
   }
 }
 
@@ -31,48 +31,37 @@ void setPx(int x, int y, bool val) {
 }
 
 void drawBuffer() {
-  for(int i=0; i<52; i+=2) {
+  for(int i=0; i<52; i+=1) {
     if (i >= MIN_X && i < MAX_X && lineCount >= MIN_Y && lineCount < MAX_Y) {
       int mx = i - MIN_X;
       int my = HEIGHT * (lineCount - MIN_Y) / (MAX_Y - MIN_Y);
-//      analogWrite(VID_PIN, dst_buf[mx + my * WIDTH] ? 0 : 255);
-      digitalWrite(VID_PIN, dst_buf[mx + my * WIDTH] ? HIGH : LOW);
+      digitalWriteFast(VID_PIN, dst_buf[mx + my * WIDTH]);
     }
     else {
-      digitalWrite(VID_PIN, LOW);
+      digitalWriteFast(VID_PIN, LOW);
     }
     
-    delayMicroseconds(2);
+    delayMicroseconds(1);
   }
-}
-
-void setup() {
-  pinMode(VID_PIN,  OUTPUT);
-  pinMode(SYNC_PIN, OUTPUT);
-  setupBuffer();
-  analogWriteFrequency(VID_PIN, 375000);
-//  setPx(10, 10, false);
-//  setPx(31, 1, false);
-//  setPx(32, 2, false);
 }
 
 void scanline() {
   // sync
-  digitalWrite(VID_PIN, LOW);
-  digitalWrite(SYNC_PIN,LOW);
+  digitalWriteFast(VID_PIN, LOW);
+  digitalWriteFast(SYNC_PIN,LOW);
   delayMicroseconds(5);
 
   // back porch
-  digitalWrite(VID_PIN,  LOW);
-  digitalWrite(SYNC_PIN,HIGH);
+  digitalWriteFast(VID_PIN,  LOW);
+  digitalWriteFast(SYNC_PIN,HIGH);
   delayMicroseconds(4);
 
   // picture
   drawBuffer();
 
   // front porch
-  digitalWrite(VID_PIN,  LOW);
-  digitalWrite(SYNC_PIN,HIGH);
+  digitalWriteFast(VID_PIN,  LOW);
+  digitalWriteFast(SYNC_PIN,HIGH);
   delayMicroseconds(1);
 }
 
@@ -80,59 +69,66 @@ void vsync() {
   // 9 pulse frames
   // 6 pre equalizing pulses
   for(int i=0; i<5; i++) {
-    digitalWrite(VID_PIN, LOW);
-    digitalWrite(SYNC_PIN,LOW);
+    digitalWriteFast(VID_PIN, LOW);
+    digitalWriteFast(SYNC_PIN,LOW);
     delayMicroseconds(5);
-    digitalWrite(VID_PIN,  LOW);
-    digitalWrite(SYNC_PIN,HIGH);
+    digitalWriteFast(VID_PIN,  LOW);
+    digitalWriteFast(SYNC_PIN,HIGH);
     delayMicroseconds(27);
   }
   // 6 vertical sync pulses
   for(int i=0; i<5; i++) {
-    digitalWrite(VID_PIN,  LOW);
-    digitalWrite(SYNC_PIN,HIGH);
+    digitalWriteFast(VID_PIN,  LOW);
+    digitalWriteFast(SYNC_PIN,HIGH);
     delayMicroseconds(2);
-    digitalWrite(VID_PIN, LOW);
-    digitalWrite(SYNC_PIN,LOW);
+    digitalWriteFast(VID_PIN, LOW);
+    digitalWriteFast(SYNC_PIN,LOW);
     delayMicroseconds(30);
   }
   // 6 post equalizing pulses
   for(int i=0; i<5; i++) {
-    digitalWrite(VID_PIN, LOW);
-    digitalWrite(SYNC_PIN,LOW);
+    digitalWriteFast(VID_PIN, LOW);
+    digitalWriteFast(SYNC_PIN,LOW);
     delayMicroseconds(2);
-    digitalWrite(VID_PIN,  LOW);
-    digitalWrite(SYNC_PIN,HIGH);
+    digitalWriteFast(VID_PIN,  LOW);
+    digitalWriteFast(SYNC_PIN,HIGH);
     delayMicroseconds(30);
   }
   // 11 black frames
   for(int i=0; i<10; i++) {
-    digitalWrite(VID_PIN, LOW);
-    digitalWrite(SYNC_PIN,LOW);
+    digitalWriteFast(VID_PIN, LOW);
+    digitalWriteFast(SYNC_PIN,LOW);
     delayMicroseconds(5);
-    digitalWrite(VID_PIN,  LOW);
-    digitalWrite(SYNC_PIN,HIGH);
+    digitalWriteFast(VID_PIN,  LOW);
+    digitalWriteFast(SYNC_PIN,HIGH);
     delayMicroseconds(58);
   }
 }
 
 void halfline() {
   // just treating as filler for now
-  digitalWrite(VID_PIN, LOW);
-  digitalWrite(SYNC_PIN,LOW);
+  digitalWriteFast(VID_PIN, LOW);
+  digitalWriteFast(SYNC_PIN,LOW);
   delayMicroseconds(5);
-  digitalWrite(VID_PIN,  LOW);
-  digitalWrite(SYNC_PIN,HIGH);
+  digitalWriteFast(VID_PIN,  LOW);
+  digitalWriteFast(SYNC_PIN,HIGH);
   delayMicroseconds(26);
 }
 
-void loop() {
-  vsync();
-  for(lineCount=0; lineCount<241; lineCount++) {
-    scanline();
-  }
-  halfline();
-  evenFrame = !evenFrame;
-  frameCount++;
+void setup() {
+  pinMode(VID_PIN,  OUTPUT);
+  pinMode(SYNC_PIN, OUTPUT);
   setupBuffer();
+}
+
+void loop() {
+  while(1) {
+    vsync();
+    for(lineCount=0; lineCount<241; lineCount++) {
+      scanline();
+    }
+    halfline();
+    evenFrame = !evenFrame;
+    frameCount++;
+  }
 }
