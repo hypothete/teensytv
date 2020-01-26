319e127ed9bf65cc18873a55f19d873cd4a14da0
Duncan Alexander <hypo@hypothete.com>
2020-01-26 13:52:30 -0800

    checkerboard test

--- tv-test.ino
+++ tv-test.ino
@@ -1,28 +1,26 @@
 #define VID_PIN 15
 #define SYNC_PIN 14
-#define MIN_X 2
-#define MIN_Y 8
-#define MAX_X 50
-#define MAX_Y 240
-#define WIDTH 48
-#define HEIGHT 29
+
+#define WIDTH 52
+#define HEIGHT 242
+#define VHEIGHT 39
 
 int lineCount = 0;
 bool evenFrame = true;
 int frameCount = 0;
-bool * dst_buf = (bool*)malloc(WIDTH*HEIGHT);
+bool * dst_buf = (bool*)malloc(WIDTH * VHEIGHT);
 
 void setupBuffer() {
-  for(int i=0; i<WIDTH*HEIGHT; i++) {
+  for(int i=0; i< WIDTH * VHEIGHT; i++) {
     int x = i % WIDTH;
     int y = i / WIDTH;
-//    dst_buf[i] = (8.0 * cos(y) - 8.0 * sin((float)(x - frameCount / 8.0))) > 0;
-    int H2 = HEIGHT / 2;
+    int H2 = VHEIGHT / 2;
     int W2 = WIDTH / 2;
     int dx = W2 - x;
     int dy = H2 - y;
     int hypo = sqrt(dx*dx + dy*dy);
-    dst_buf[i] = hypo < 12; //16 * (sin((float)(frameCount / 32.0)) + 1);
+    dst_buf[i] = x % 2 ^ y % 2;
+//    dst_buf[i] = hypo < H2 * (sin((float)(frameCount / 32.0)) + 1);
   }
 }
 
@@ -31,16 +29,9 @@ void setPx(int x, int y, bool val) {
 }
 
 void drawBuffer() {
-  for(int i=0; i<52; i+=1) {
-    if (i >= MIN_X && i < MAX_X && lineCount >= MIN_Y && lineCount < MAX_Y) {
-      int mx = i - MIN_X;
-      int my = HEIGHT * (lineCount - MIN_Y) / (MAX_Y - MIN_Y);
-      digitalWriteFast(VID_PIN, dst_buf[mx + my * WIDTH]);
-    }
-    else {
-      digitalWriteFast(VID_PIN, LOW);
-    }
-    
+  for(int i=0; i < WIDTH; i++) {
+    int y = VHEIGHT * lineCount / HEIGHT;
+    digitalWriteFast(VID_PIN, dst_buf[i + y * WIDTH]);
     delayMicroseconds(1);
   }
 }
@@ -66,40 +57,33 @@ void scanline() {
 }
 
 void vsync() {
+  digitalWriteFast(VID_PIN,  LOW);
   // 9 pulse frames
   // 6 pre equalizing pulses
   for(int i=0; i<5; i++) {
-    digitalWriteFast(VID_PIN, LOW);
     digitalWriteFast(SYNC_PIN,LOW);
     delayMicroseconds(5);
-    digitalWriteFast(VID_PIN,  LOW);
     digitalWriteFast(SYNC_PIN,HIGH);
     delayMicroseconds(27);
   }
   // 6 vertical sync pulses
   for(int i=0; i<5; i++) {
-    digitalWriteFast(VID_PIN,  LOW);
     digitalWriteFast(SYNC_PIN,HIGH);
     delayMicroseconds(2);
-    digitalWriteFast(VID_PIN, LOW);
     digitalWriteFast(SYNC_PIN,LOW);
     delayMicroseconds(30);
   }
   // 6 post equalizing pulses
   for(int i=0; i<5; i++) {
-    digitalWriteFast(VID_PIN, LOW);
     digitalWriteFast(SYNC_PIN,LOW);
     delayMicroseconds(2);
-    digitalWriteFast(VID_PIN,  LOW);
     digitalWriteFast(SYNC_PIN,HIGH);
     delayMicroseconds(30);
   }
   // 11 black frames
   for(int i=0; i<10; i++) {
-    digitalWriteFast(VID_PIN, LOW);
     digitalWriteFast(SYNC_PIN,LOW);
     delayMicroseconds(5);
-    digitalWriteFast(VID_PIN,  LOW);
     digitalWriteFast(SYNC_PIN,HIGH);
     delayMicroseconds(58);
   }
@@ -110,9 +94,8 @@ void halfline() {
   digitalWriteFast(VID_PIN, LOW);
   digitalWriteFast(SYNC_PIN,LOW);
   delayMicroseconds(5);
-  digitalWriteFast(VID_PIN,  LOW);
   digitalWriteFast(SYNC_PIN,HIGH);
-  delayMicroseconds(26);
+  delayMicroseconds(2);
 }
 
 void setup() {
@@ -124,11 +107,12 @@ void setup() {
 void loop() {
   while(1) {
     vsync();
-    for(lineCount=0; lineCount<241; lineCount++) {
+    for(lineCount=0; lineCount < HEIGHT; lineCount++) {
       scanline();
     }
     halfline();
     evenFrame = !evenFrame;
     frameCount++;
+    setupBuffer();
   }
 }
